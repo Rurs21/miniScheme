74cb6ad9ac8ee13846e4eb8bca3419dbe947c738
Rurs21 <charles-emile96@hotmail.com>
2020-06-03 00:31:34 -0400

    Add fileToString function

--- src/main/java/miniScheme/App.java
+++ src/main/java/miniScheme/App.java
@@ -1,10 +1,26 @@
 package miniScheme;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    private static String fileToString(String path) {
+        String content = "";
+        try {
+            content = Files.readString(Paths.get(path), StandardCharsets.UTF_8);
+        } catch (IOException | OutOfMemoryError | SecurityException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e ) {
+            System.err.println("FUCK:" + e.getMessage());
+        }
+        return content;
     }
 }
